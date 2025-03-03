019bbd028b5139b809ccdd41b7e1de7f519ccc15
AlekseiGreen <spacegoogls@gmail.com>
2025-03-03 15:25:45 +1000

    Add

--- src/components/Logic.jsx
+++ src/components/Logic.jsx
@@ -6,7 +6,7 @@ import "./Logic.css";
 
 const Logic = () => {
     const [isDragging, setIsDragging] = useState(false);
-    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [position, setPosition] = useState({ x: 0, y: 22 });
     const [offset, setOffset] = useState({ x: 0, y: 0 });
   
     // Начало перетаскивания
